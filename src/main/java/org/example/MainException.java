d27900f6813b032c88136dcff8196efd936699cd
liviu <lupascu.liviu@yahoo.com>
2024-11-17 13:54:23 +0200

    varianta finala

--- src/main/java/org/example/MainException.java
+++ src/main/java/org/example/MainException.java
@@ -18,22 +18,28 @@ public class MainException {
 
         while (restartFlag) {
 
+            //initiem dialogul
             System.out.println("Curs infoAcademy = Lectia 8 - Exercitiu 2: \n Sa incepem! \n introduceti numele: ");
             name = scanner.nextLine();
 
+            //prevalidarea numelui
             while (name.isEmpty()) {
-
                 System.out.println("numele introdus e vid, va rugam introuceti o valoare de minim 2 litere. \n" +
                         "Programul nu va continua fara datele corecte");
-                if (regexName.matcher(name).find()) {
-                    throw new RuntimeException("numele poate poate contine doar litere " +
-                            "nu poate contine caractere speciale sau cifre");
-                }
                 System.out.println("va rugam sa reintroduceti numele: ");
                 name = scanner.nextLine();
             }
-
-
+            while(!name.isEmpty()){
+                if (regexName.matcher(name).find()) {
+                    System.out.println("numele normale nu pot contine doar litere! " +
+                            "valoarea introdusa nu poate contine caractere speciale sau cifre");
+                    System.out.println("va rugam sa reintroduceti numele: ");
+                    name = scanner.nextLine();
+                }else{
+                    break;
+                }
+            }
+            //prevalidatea varstei
             System.out.println("introduceti varsta: ");
             inputAge = scanner.nextLine();
 
@@ -45,16 +51,19 @@ public class MainException {
             }
             personAge = Integer.parseInt(inputAge);
 
+            //Incercam sa populam clasa persoana cu datele preluate de la utilizator
             try {
                 Person person = new Person(name, personAge);
                 System.out.println(person);
                 System.out.println("am terminat cu " + person.getName() +  ", vreti sa mai introduceti o persoana? \n" +
+                        "***************************************************************************** \n" +
                         "tastati 'Y' pt a introduce o alta persoana, sau 'N' pt a iesi \n" +
                         "*****************************************************************************");
             } catch (NegativeAgeException unborn) {
-                System.out.println(unborn.getMessage());
+                throw new NegativeAgeException();
 
             }
+            //Daca totul e OK repornim bucla
             repornire = scanner.nextLine();
 
             while (repornire.isEmpty()) {
